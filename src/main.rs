edcf4a957f159b6f9bb2c52463552b52bf95e803
jus1d <artfa63@gmail.com>
2025-04-13 00:37:55 +0400

    chore: parse command line args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::fs;
+use std::process;
 
 pub mod lexer;
 pub mod parser;
 
 fn main() {
-    let filename = "main.w";
-    let source = fs::read_to_string(filename).expect("Failed to read file");
+    let mut args = std::env::args();
+    let program = args.next().unwrap();
+    let filename = args.next().unwrap_or_else(|| {
+        eprintln!("Usage: {} <filename>", program);
+        eprintln!("error: no file path provided");
+        process::exit(1);
+    });
+
+    let source = fs::read_to_string(filename.clone()).expect("Failed to read file");
 
     let lexer = lexer::Lexer::new(source.chars(), filename.to_string());
     let mut parser = parser::Parser::from_iter(lexer);
