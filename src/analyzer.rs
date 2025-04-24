339ed628c757b51babbb2e3c4b1837e3ae929020
jus1d <artfa63@gmail.com>
2025-04-24 13:28:32 +0400

    chore: Allow calling user-defined functions as a statements

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,5 +1,6 @@
-use crate::{diag, parser};
 use std::collections::HashMap;
+use crate::parser::Statement;
+use crate::{diag, parser};
 
 pub fn check_collisions_with_builtin(program: &parser::Program) {
     let builtins = HashMap::from([
@@ -30,16 +31,27 @@ pub fn check_collisions_with_builtin(program: &parser::Program) {
 
         for statement in &function.body {
             match statement {
-                parser::Statement::Funcall { name, args } => {
+                Statement::Funcall { name, args } => {
+                    // TODO: factor args checking into a separate function
                     if builtins.contains_key(name.as_str()) {
-                        if args.len() > builtins[name.as_str()].params.len() {
+                        let params_count = builtins[name.as_str()].params.len();
+                        if args.len() > params_count {
                             diag::fatal!("too many arguments to function call '{name}', expected {} arguments, have {}", builtins[name.as_str()].params.len(), args.len());
-                        } else if args.len() < builtins[name.as_str()].params.len() {
+                        } else if args.len() < params_count {
                             diag::fatal!("too few arguments to function call '{name}', expected {} arguments, have {}", builtins[name.as_str()].params.len(), args.len());
                         }
 
                         // TODO: type check function arguments
                     } else if !program.functions.contains_key(name.as_str()) {
+                        let params_count = program.functions[name.as_str()].params.len();
+                        if args.len() > params_count {
+                            diag::fatal!("too many arguments to function call '{name}', expected {} arguments, have {}", builtins[name.as_str()].params.len(), args.len());
+                        } else if args.len() < params_count {
+                            diag::fatal!("too few arguments to function call '{name}', expected {} arguments, have {}", builtins[name.as_str()].params.len(), args.len());
+                        }
+
+                        // TODO: type check function arguments
+                    } else {
                         diag::fatal!("call to undeclared function '{name}'");
                     }
                 }
