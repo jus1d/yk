6dacf0badc43be559e1660324eedf3f4c656711f
jus1d <artfa63@gmail.com>
2025-04-26 16:11:10 +0400

    fix: Move arguments into stack

--- src/compiler/aarch64.rs
+++ src/compiler/aarch64.rs
@@ -16,7 +16,7 @@ pub fn generate_aarch64_darwin_assembly<W: Write>(ast: &Ast, out: &mut W) -> io:
     generate_aarch64_darwin_exit(out)?;
 
     for (_, function) in &ast.functions {
-        generate_aarch64_darwin_func_body(&ast, out, &mut strings, function)?;
+        generate_aarch64_darwin_func_body(out, &ast, &mut strings, function)?;
     }
     generate_aarch64_darwin_data_section(out, &strings)?;
     Ok(())
@@ -29,30 +29,42 @@ fn generate_aarch64_darwin_preamble<W: Write>(out: &mut W) -> io::Result<()> {
     Ok(())
 }
 
-fn generate_aarch64_darwin_func_prologue<W: Write>(out: &mut W) -> io::Result<()> {
+// offset in quadwords (64 bits)
+fn generate_aarch64_darwin_func_prologue<W: Write>(out: &mut W, mut offset: usize) -> io::Result<()> {
+    if offset % 2 == 1 {
+        offset += 1;
+    }
+
+    let stack_offset = 16 + (offset * 8);
     writeln!(out, "    ; prologue")?;
-    writeln!(out, "    stp     x29, x30, [sp, -16]!")?;
+    writeln!(out, "    stp     x29, x30, [sp, -{}]!", stack_offset)?;
     writeln!(out, "    mov     x29, sp")?;
     Ok(())
 }
 
-fn generate_aarch64_darwin_func_body<W: Write>(ast: &Ast, out: &mut W, strings: &mut Vec<String>, function: &Function) -> io::Result<()> {
+fn generate_aarch64_darwin_func_body<W: Write>(out: &mut W, ast: &Ast, strings: &mut Vec<String>, function: &Function) -> io::Result<()> {
     writeln!(out, "; function {}", function.name)?;
     writeln!(out, "_{}:", function.name)?;
 
-    generate_aarch64_darwin_func_prologue(out)?;
+    generate_aarch64_darwin_func_prologue(out, function.params.len())?;
     for statement in &function.body {
-        generate_aarch64_darwin_statement(ast, out, statement, strings, &function.name)?;
+        generate_aarch64_darwin_statement(out, ast, function, statement, strings)?;
     }
     writeln!(out)?;
 
     Ok(())
 }
 
-fn generate_aarch64_darwin_func_epilogue<W: Write>(out: &mut W) -> io::Result<()> {
+// offset in quadwords (64 bits)
+fn generate_aarch64_darwin_func_epilogue<W: Write>(out: &mut W, mut offset: usize) -> io::Result<()> {
+    if offset % 2 == 1 {
+        offset += 1;
+    }
+
+    let stack_offset = 16 + (offset * 8);
     writeln!(out, "    ; epilogue")?;
     writeln!(out, "    mov     sp, x29")?;
-    writeln!(out, "    ldp     x29, x30, [sp], 16")?;
+    writeln!(out, "    ldp     x29, x30, [sp], {}", stack_offset)?;
     writeln!(out, "    ret")?;
     Ok(())
 }
@@ -66,7 +78,7 @@ fn generate_aarch64_darwin_data_section<W: Write>(out: &mut W, strings: &Vec<Str
     writeln!(out, "; data section")?;
     for (i, s) in strings.iter().enumerate() {
         writeln!(out, "string.{}:", i)?;
-        writeln!(out, "    .ascii \"{}\\n\"", s)?;
+        writeln!(out, "    .asciz \"{}\\n\"", s)?;
     }
     Ok(())
 }
@@ -97,34 +109,44 @@ fn generate_aarch64_darwin_exit<W: Write>(out: &mut W) -> io::Result<()> {
     Ok(())
 }
 
-fn generate_aarch64_darwin_statement<W: Write>(ast: &Ast, out: &mut W, statement: &Statement, strings: &mut Vec<String>, current_func_name: &str) -> io::Result<()> {
+fn generate_aarch64_darwin_statement<W: Write>(out: &mut W, ast: &Ast, func: &Function, statement: &Statement, strings: &mut Vec<String>) -> io::Result<()> {
     match statement {
         Statement::Ret { value } => {
             if let Some(expr) = value {
-                generate_aarch64_darwin_expression(ast, out, &expr, strings, current_func_name, "x0")?;
+                generate_aarch64_darwin_expression(out, ast, &expr, strings, &func.name, "x0")?;
             }
-            generate_aarch64_darwin_func_epilogue(out)?;
+            generate_aarch64_darwin_func_epilogue(out, func.params.len())?;
         }
         Statement::Funcall { name, args } => {
+            // TODO: save arguments to stack only if next function polutes x0-x7 registers
+            writeln!(out, "    ; store arguments of {}", func.name)?;
+            for i in 0..func.params.len() {
+                writeln!(out, "    str     x{}, [x29, {}]", i, 16 + 8 * i)?;
+            }
+            writeln!(out, "    ; load arguments for {}", name)?;
             for (i, arg) in args.iter().enumerate() {
                 let reg = format!("x{}", i);
-                generate_aarch64_darwin_expression(ast, out, arg, strings, current_func_name, &reg)?;
+                generate_aarch64_darwin_expression(out, ast, arg, strings, &func.name, &reg)?;
             }
             writeln!(out, "    ; call {}", name)?;
             writeln!(out, "    bl      _{}", name)?;
+            writeln!(out, "    ; restore arguments for {}", func.name)?;
+            for i in 0..func.params.len() {
+                writeln!(out, "    ldr     x{}, [x29, {}]", i, 16 + 8 * i)?;
+            }
         }
     }
     Ok(())
 }
 
-fn generate_aarch64_darwin_expression<W: Write>(ast: &Ast, out: &mut W, expr: &Expr, strings: &mut Vec<String>, current_func_name: &str, register: &str) -> io::Result<()> {
+fn generate_aarch64_darwin_expression<W: Write>(out: &mut W, ast: &Ast, expr: &Expr, strings: &mut Vec<String>, current_func_name: &str, register: &str) -> io::Result<()> {
     match expr {
         Expr::Literal(lit) => {
             generate_aarch64_darwin_literal(out, lit, strings, register)?;
         }
         Expr::Binary { op, lhs, rhs } => {
-            generate_aarch64_darwin_expression(ast, out, lhs, strings, current_func_name, "x9")?;
-            generate_aarch64_darwin_expression(ast, out, rhs, strings, current_func_name, "x10")?;
+            generate_aarch64_darwin_expression(out, ast, lhs, strings, current_func_name, "x9")?;
+            generate_aarch64_darwin_expression(out, ast, rhs, strings, current_func_name, "x10")?;
             writeln!(out, "    ; binop: {} {} {}", lhs, op, rhs)?;
             match op {
                 BinaryOp::Add => writeln!(out, "    add     {}, x10, x9", register)?,
@@ -137,7 +159,7 @@ fn generate_aarch64_darwin_expression<W: Write>(ast: &Ast, out: &mut W, expr: &E
             writeln!(out, "    ; args for funcall {}", name)?;
             for (i, arg) in args.iter().enumerate() {
                 let register = format!("x{}", i);
-                generate_aarch64_darwin_expression(ast, out, arg, strings, current_func_name, &register)?;
+                generate_aarch64_darwin_expression(out, ast, arg, strings, current_func_name, &register)?;
             }
             writeln!(out, "    ; call {}", name)?;
             writeln!(out, "    bl      _{}", name)?;
@@ -164,8 +186,8 @@ fn generate_aarch64_darwin_literal<W: Write>(out: &mut W, lit: &Literal, strings
             let idx = strings.len();
             strings.push(text.to_string());
             writeln!(out, "    ; string: \"{}\"", text)?;
-            writeln!(out, "    adrp    x0, string.{}@PAGE", idx)?;
-            writeln!(out, "    add     x0, x0, string.{}@PAGEOFF", idx)?;
+            writeln!(out, "    adrp    {}, string.{}@PAGE", register, idx)?;
+            writeln!(out, "    add     {}, {}, string.{}@PAGEOFF", register, register, idx)?;
         },
     }
     Ok(())
